1a893622743a1e8d89e90ecde1a8d7d746d70432
raelbarros <israel.silva.barros@hotmail.com>
2019-11-14 08:09:40 -0300

    update

--- NavigationViewTest/app/src/main/java/br/senac/navigationviewtest/MainActivity.java
+++ NavigationViewTest/app/src/main/java/br/senac/navigationviewtest/MainActivity.java
@@ -1,11 +1,13 @@
 package br.senac.navigationviewtest;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
@@ -13,7 +15,7 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
-    private ActionBarDrawerToggle btnSanduiche;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
                 if (menuItem.isChecked()){
                     menuItem.setChecked(false);
                 } else {
@@ -42,21 +44,38 @@ public class MainActivity extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, fragmento01).commit();
                     return true;
                 }
+
                 return false;
             }
         });
 
-        btnSanduiche = new ActionBarDrawerToggle(this, drawerLayout, R.string.openDrawer, R.string.closeDrawer);
-        drawerLayout.addDrawerListener(btnSanduiche);
-        btnSanduiche.syncState();
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.openDrawer, R.string.closeDrawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
     }
 
+    // Verifica qual menu foi selecionado
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        if (btnSanduiche.onOptionsItemSelected(item)) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else if (item.getItemId() == R.id.action_messages) {
+            Intent intent = new Intent(MainActivity.this, MessagesActivity.class);
+            startActivity(intent);
+            return true;
+
+        } else if (item.getItemId() == R.id.action_home) {
+            Intent intent = new Intent(MainActivity.this, Home.class);
+            startActivity(intent);
             return true;
         }
         return false;
+    }
+
+    //cria menu toolbar
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
     }
 }
