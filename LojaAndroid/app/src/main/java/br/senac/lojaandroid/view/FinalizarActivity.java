2ca36c5b918577fd5e7bf543f9ca18bb73d5e107
raelbarros <israel.silva.barros@hotmail.com>
2019-11-28 15:08:34 -0300

    tela finalizar compra

---
+++ LojaAndroid/app/src/main/java/br/senac/lojaandroid/view/FinalizarActivity.java
@@ -0,0 +1,16 @@
+package br.senac.lojaandroid.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.senac.lojaandroid.R;
+
+public class FinalizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalizar);
+    }
+}
